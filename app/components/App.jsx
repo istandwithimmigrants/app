59feca69e6c120e4968a1e0f74dcb53f38f5b548
Nitika Nadgar <nitiquita@gmail.com>
2017-07-15 12:18:10 -0400

    some functionality for question number

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import RaisedButton from 'material-ui/RaisedButton'
+import {Link} from 'react-router'
 
 import Login from './Login'
 
@@ -7,14 +9,31 @@ import TextField from 'material-ui/TextField'
 import AppBar from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
 
-const App = ({children}) => (
-  <div>
-    <AppBar
-      iconElementRight={<Login />}
-      iconElementLeft={<TextField style={{backgroundColor: 'white', width: 140}} hintText=" Question Number" />}
-      />
-    {children}
-  </div>
-)
+const style = {
+  margin: 12
+}
 
-export default App
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      qNum: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({qNum: event.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          iconElementRight={<Login />}
+          iconElementLeft={<div><TextField style={{ backgroundColor: 'white', width: 140 }} hintText=" Question Number" onChange={this.handleChange}/><Link to={`/question${this.state.qNum}`}><RaisedButton label="Take me to my question" style={style}/></Link></div>}
+        />
+        {this.props.children}
+      </div>
+    )
+  }
+}
