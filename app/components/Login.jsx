b5fcad5d728df7dcf573ccde886835a2c6ac8ef5
Rachel Cohen <rfc@alumni.duke.edu>
2017-07-15 11:53:10 -0400

    fixed login

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,15 +1,80 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router'
 
-export const Login = ({ login }) => (
-  <form onSubmit={evt => {
+import FlatButton from 'material-ui/FlatButton'
+import Dialog from 'material-ui/Dialog'
+import TextField from 'material-ui/TextField'
+
+class Login extends Component {
+  state = {
+    open: false
+  }
+
+  handleOpen = () => {
+    this.setState({ open: true })
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    })
+  }
+
+  handleSubmit = evt => {
     evt.preventDefault()
-    login(evt.target.username.value, evt.target.password.value)
-  } }>
-    <input name="username" />
-    <input name="password" type="password" />
-    <input type="submit" value="Login" />
-  </form>
-)
+    const email = evt.target.email.value
+    const password = evt.target.password.value
+    this.props.login(email, password)
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <Link to='lawyer' >
+        <FlatButton
+          label="Submit"
+          type="submit"
+          form="login"
+          primary={true}
+          keyboardFocused={true}
+          onTouchTap={this.handleClose}
+        />
+      </Link>,
+    ]
+    return (
+      <div>
+        <FlatButton
+          label="Lawyer Login"
+          onTouchTap={this.handleOpen}
+          style={{color: 'white'}} />
+        <Dialog
+          title="Login"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}>
+          <form id="login" onSubmit={this.handleSubmit}>
+            <TextField
+              name="email"
+              hintText="Email Address"
+            /><br />
+            <br />
+            <TextField
+              name="password"
+              type="password"
+              hintText="Password"
+            /><br />
+            <br />
+          </form>
+        </Dialog>
+      </div>
+    )
+  }
+}
 
 import {login} from 'APP/app/reducers/auth'
 import {connect} from 'react-redux'
